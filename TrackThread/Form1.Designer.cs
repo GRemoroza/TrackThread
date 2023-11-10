14ef703c1a728191f93baf0fc72d0f66f2ebcd6b
Gerald Remoroza <remoroza.254068@balagtas.sti.edu.ph>
2023-11-10 21:39:51 +0800

    Add project files.

---
+++ TrackThread/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace TrackThread
+{
+    partial class frmTrackThread
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "---Thread Starts---";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(136, 110);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(101, 39);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // frmTrackThread
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 200);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.label1);
+            this.Name = "frmTrackThread";
+            this.Text = "frmTrackThread";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
